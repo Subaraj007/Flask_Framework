555b741ae23af2a36a692a48a4c5bc96e603c818
Subaraj <subaraj1961@gmail.com>
2024-02-16 16:36:19 +0530

    First Webdevelopment

--- app.py
+++ app.py
@@ -6,5 +6,14 @@ app = Flask(__name__, template_folder='templates')  # Specify the templates fold
 def index():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')  #  an 'about.html' template
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name) #for user.html template
+
+
 if __name__ == "__main__":
     app.run(debug=True)
